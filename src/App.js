caff345a54f1f56a0967ac9b88e652ac9c23ab07
Mark Weston <mweston@inviqa.com>
2022-10-11 15:49:32 +0100

    Test LastFM API access

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import SelectMusic from "./SelectMusic";
 function App() {
   return (
     <div className="music-selector">
+      <a href="http://www.last.fm/api/auth/?api_key=1af931d4ae450156c121bc0b8db3f973&cb=https://music-selector-47maoj40t-markweston.vercel.app">
+        Allow Access
+      </a>
       <header>
         <h1>My Music Selector</h1>
         <h2>What shall I listen to now?</h2>
