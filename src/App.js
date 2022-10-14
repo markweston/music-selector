dec3fdb9e6cd82e4fd589043111aa9d05e46e242
Mark Weston <mweston@inviqa.com>
2022-10-14 11:34:40 +0100

    Remove API token from source

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SelectMusic from "./SelectMusic";
 function App() {
   return (
     <div className="music-selector">
-      <a href="http://www.last.fm/api/auth/?api_key=1af931d4ae450156c121bc0b8db3f973&cb=https://music-selector-git-develop-markweston.vercel.app/">
+      <a href="http://www.last.fm/api/auth/?api_key={process.env.REACT_APP_TOKEN}&cb=https://music-selector-git-develop-markweston.vercel.app/">
         Allow Access
       </a>
       <header>
